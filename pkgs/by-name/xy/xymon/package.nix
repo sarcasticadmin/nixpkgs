6a072aa095761880e0007008642348e86d0cf0bb
Robert James Hernandez <rob@sarcasticadmin.com>
2023-09-26 10:59:55 +0000

    wip

--- pkgs/by-name/xy/xymon/package.nix
+++ pkgs/by-name/xy/xymon/package.nix
@@ -7,21 +7,33 @@
 , c-ares
 , rrdtool
 , openssl
+, libtirpc
+, ntirpc
+, fping
+, breakpointHook
 }:
 
 stdenv.mkDerivation rec {
   pname = "xymon";
   version = "4.4-alpha";
 
-  buildInputs = [ sqlite pcre zlib c-ares rrdtool openssl ];
+  nativeBuildInputs = [ breakpointHook ];
+
+  buildInputs = [ sqlite pcre zlib c-ares rrdtool openssl libtirpc ntirpc fping ];
+
+  XYMONUSER="nobody";
+  #USEXYMONPING="y";
+  #configurePhase = ''
+  #  ./configure --server --fping ${fping}/bin/fping
+  #'';
   configurePhase = ''
     ./configure --server
   '';
 
   src = fetchsvn {
-    url = "svn://svn.code.sf.net/p/xymon/code/trunk";
+    url = "svn://svn.code.sf.net/p/xymon/code/branches/4.x-master";
     rev = "8120";
-    sha256 = "sha256-kRY3qtPZhsibSK+cDb6Mmrvhk66wIZDKf+TrhVV1nas=";
+    sha256 = "sha256-3mJgnNaYdov5GiS//O58LupHF1ohXqW0fT67zvniw7s=";
   };
 
   meta = with lib; {
