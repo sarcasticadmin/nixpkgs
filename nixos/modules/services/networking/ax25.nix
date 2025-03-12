d92fb40ce49d3d06427d84c5fe8f9d8349713802
Robert James Hernandez <rob@sarcasticadmin.com>
2025-03-12 07:22:55 +0000

    wip

---
+++ nixos/modules/services/networking/ax25.nix
@@ -0,0 +1,70 @@
+{ config, lib, pkgs, ... }:
+
+let
+
+  inherit (lib)
+    types
+    ;
+
+  inherit (lib.modules)
+    mkIf
+    ;
+
+  inherit (lib.options)
+    mkEnableOption
+    literalExpression
+    mkOption
+    ;
+
+  cfg = config.services.ax25;
+
+  ax25ToolsPkg = cfg.package;
+
+  kissScript = pkgs.writeScript "kissScript" ''
+    #!${pkgs.runtimeShell}
+    ${ax25ToolsPkg}/bin/kissattach ${cfg.tty} ${cfg.port}
+    ${ax25ToolsPkg}/bin/kissparms -p ${cfg.port} ${cfg.extraKISSParams}
+  '';
+in
+{
+
+  options = {
+    services.ax25 = {
+      enable = mkEnableOption (lib.mdDoc "ax25 init tnc");
+
+      package = mkOption {
+        type = types.package;
+        default = pkgs.ax25-tools;
+        defaultText = literalExpression "pkgs.ax25-tools";
+        description = lib.mdDoc "The ax25-tools package to use.";
+      };
+
+      # need to add ax25 config with buad rate, etc.
+
+      tty = mkOption {
+				type = types.str;
+				default = "/dev/ttyACM0";
+      };
+
+      port = mkOption {
+				type = types.str;
+				default = "wl2k";
+      };
+
+      extraKISSParams = mkOption {
+				type = types.str;
+				default = "-t 300 -l 10 -s 12 -r 80 -f n";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    systemd.services.ax25-init = {
+      description = "AX.25 init KISS interface";
+      after = [ "network.target" ];
+      #serviceConfig.Type = "forking";
+      serviceConfig.Type = "simple";
+      serviceConfig.ExecStart = "${kissScript}";
+    };
+  };
+}
