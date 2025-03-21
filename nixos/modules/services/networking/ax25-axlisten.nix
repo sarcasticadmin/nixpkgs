8a0eaeaa36c79e87ccefd336129298d53af1467d
Robert James Hernandez <rob@sarcasticadmin.com>
2025-03-21 23:00:11 +0000

    axlisten

---
+++ nixos/modules/services/networking/ax25-axlisten.nix
@@ -0,0 +1,54 @@
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (lib)
+    types
+    ;
+
+  inherit (lib.modules)
+    mkIf
+    ;
+
+  inherit (lib.options)
+    mkEnableOption
+    literalExpression
+    mkOption
+    ;
+
+  cfg = config.services.axlisten;
+in
+{
+  options = {
+
+    services.ax25.axlisten = {
+
+      enable = mkEnableOption (lib.mdDoc "AX.25 axlisten daemon");
+
+      package = mkOption {
+        type = types.package;
+        default = pkgs.ax25-apps;
+        defaultText = literalExpression "pkgs.ax25-apps";
+        description = lib.mdDoc "The ax25-apps package to use.";
+      };
+
+      config = mkOption {
+				type = types.str;
+        description = lib.mdDoc "config flags for axlisten";
+				default = "-art";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+
+    systemd.services.axlisten =
+      {
+        description = "AX.25 traffic monitor";
+        after = [ "network.target" ];
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = "${cfg.package}/bin/axlisten ${cfg.config}";
+        };
+      };
+  };
+}
