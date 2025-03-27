ec5838da405d408bc01e5dc9f7807c6b6c19ee24
Robert James Hernandez <rob@sarcasticadmin.com>
2025-03-27 03:40:52 +0000

    support /etc/ax25/axports

--- nixos/modules/services/networking/ax25-kissattach.nix
+++ nixos/modules/services/networking/ax25-kissattach.nix
@@ -36,9 +36,8 @@ in
         description = lib.mdDoc "The ax25-tools package to use.";
       };
 
-      # need to add ax25 config with buad rate, etc.
-
       tty = mkOption {
+        # maybe this should be types.path
 				type = types.str;
 				default = "/dev/ttyACM0";
       };
@@ -48,24 +47,53 @@ in
 				default = "tnc0";
       };
 
-      kissParams = mkOption {
+      callsign = mkOption {
 				type = types.str;
-				default = "";
+				example = "WB6WLV";
+      };
+
+      description = mkOption {
+				type = types.str;
+        default = "";
+      };
+
+      baud = mkOption {
+				type = types.int;
+				default = 57600;
+      };
+
+      paclen = mkOption {
+				type = types.int;
+				default = 255;
+      };
+
+      window = mkOption {
+				type = types.int;
+				default = 7;
+      };
+
+      kissParams = mkOption {
+				type = types.nullOr types.str;
+				default = null;
         example = "-t 300 -l 10 -s 12 -r 80 -f n";
       };
     };
   };
 
   config = mkIf cfg.enable {
+    environment.etc."ax25/axports" = {
+          text = ''
+            ${cfg.port} ${cfg.callsign} ${toString cfg.baud} ${toString cfg.paclen} ${toString cfg.window} ${cfg.description}
+          '';
+          mode = "0644";
+    };
+
     systemd.services.ax25-attach = {
       description = "AX.25 attach kiss interface";
       wantedBy = [ "multi-user.target" ];
-      #after = [ "network.target" ];
-      #after = [ "dev-${ttyName}.device" ];
-      #bindsTo = [ "dev-${ttyName}.device" ];
       serviceConfig.Type = "exec";
       serviceConfig.ExecStart = "${cfg.package}/bin/kissattach ${cfg.tty} ${cfg.port}";
-      postStart = lib.optionalString (cfg.kissParams != "") "${cfg.package}/bin/kissparms -p ${cfg.port} ${cfg.kissParams}";
+      postStart = lib.optionalString (cfg.kissParams != null) "${cfg.package}/bin/kissparms -p ${cfg.port} ${cfg.kissParams}";
     };
   };
 }
