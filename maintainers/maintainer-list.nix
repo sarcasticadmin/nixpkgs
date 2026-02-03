9e889dc9cf06be688fd08acdacf9f24d8ba1e79f
Paul Grayson <pdg@alum.mit.edu>
2026-02-03 08:59:41 -0800

    maintainers: add pdg137

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -20420,6 +20420,11 @@
     githubId = 943430;
     name = "David Hagege";
   };
+  pdg137 = {
+    name = "Paul Grayson";
+    github = "pdg137";
+    githubId = 466760;
+  };
   peat-psuwit = {
     name = "Ratchanan Srirattanamet";
     email = "peat@peat-network.xyz";
